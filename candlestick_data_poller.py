9e6fd31edae8bacc4c8f82b42ca80863edf3ec22
dnavaria <dk17816@gmail.com>
2021-11-17 13:00:15 +0530

    base code

---
+++ candlestick_data_poller.py
@@ -0,0 +1,27 @@
+import os, json
+from sqlalchemy.sql.operators import op
+from websocket import create_connection
+from dotenv import load_dotenv
+from postgres_ops import PsqlOps
+    
+load_dotenv(".env")
+PSQL_URI = os.environ.get("PSQL_URI")
+
+psql_obj = PsqlOps(PSQL_URI=PSQL_URI)
+
+ws = create_connection("wss://fx-ws.gateio.ws/v4/ws/btc")
+ws.send('{"time" : 123456, "channel" : "futures.candlesticks", "event": "subscribe", "payload" : ["1m", "BTC_USD"]}')
+
+while True:
+    data = json.loads(ws.recv())
+    print(data)
+    result = list(data["result"])
+    if result[0] == "status":
+        continue
+    for i in range(len(result)):
+        candle = result[i]
+        print(candle["t"], candle["o"], candle["c"], candle["h"], candle["l"], candle["v"], candle["n"])
+        psql_obj.save_candle(time=candle["t"], open=candle["o"], close=candle["c"], highest=candle["h"], lowest=candle["l"], volume=candle["v"], futures_contract_name=candle["n"])
+    
+    
+    
---
+++ postgres_ops.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from sqlalchemy import create_engine, exc
+from sqlalchemy import Table, Column, String, MetaData, BigInteger
+class PsqlOps:
+    def __init__(self, PSQL_URI):
+        
+        # changing postgres:// uri to postgresql:// inorder to make it work with sqlalchemy
+        if PSQL_URI and PSQL_URI.startswith("postgres://"):
+            PSQL_URI = PSQL_URI.replace("postgres://","postgresql://",1)
+        else:
+            print("Invalid psql uri found")
+            sys.exit(0)        
+            
+        self.PSQL_URI = PSQL_URI
+        
+        try:
+            # creating engine to execute queries    
+            self.db = create_engine(self.PSQL_URI)
+        except Exception as e:
+            sys.exit(f"Error occured while creating connection :: {e}")
+        
+        # making sure that a database exists before making any queries
+        try:
+            # self.db.execute("CREATE TABLE IF NOT EXISTS CANDLESTICK (time bigint, open text, close text, highest text, lowest text, volume text, future_contract_name text);")
+            meta = MetaData(self.db)
+            self.candlestick = Table('candlestick', meta, Column('time', BigInteger), Column('open', String), Column('close', String), Column('highest', String), Column('lowest', String), Column('volume', String), Column('futures_contract_name', String))
+            if not self.candlestick.exists():
+                self.candlestick.create()
+        except Exception as e:
+            sys.exit(f"Error occured while checking if database exists or not :: {e}")
+    
+    def save_candle(self, time=None, open=None, close=None, highest=None, lowest=None, volume=None, futures_contract_name=None) -> bool:
+        if time and open and close and highest and lowest and futures_contract_name:
+            try:
+                with self.db.connect() as connection:
+                    insert_query = self.candlestick.insert().values(time=time, open=open, close=close, highest=highest, lowest=lowest, volume=volume, futures_contract_name=futures_contract_name)
+                    connection.execute(insert_query)
+                return True
+            except Exception as e:
+                print(f"Exception occured while saving data to postgres db :: {e}")
+                return False
+        else:
+            print("Invalid values received")
+            return False    
+        
